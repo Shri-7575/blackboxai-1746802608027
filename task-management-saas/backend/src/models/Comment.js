005d14c2fcb0d7fc3165e038ea4d3ba4f0fe629c
Shri-7575 <shrinivashandibag@gmail.com>
2025-05-09 20:38:56 +0530

    Add task-management-saas/backend/src/models/Comment.js 1746803336315

---
+++ task-management-saas/backend/src/models/Comment.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Comment = sequelize.define('Comment', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  taskId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Tasks',
+      key: 'id'
+    }
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  attachments: {
+    type: DataTypes.ARRAY(DataTypes.JSONB),
+    defaultValue: []
+  },
+  isEdited: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  metadata: {
+    type: DataTypes.JSONB,
+    defaultValue: {}
+  }
+}, {
+  indexes: [
+    {
+      fields: ['taskId']
+    },
+    {
+      fields: ['userId']
+    }
+  ]
+});
+
+module.exports = Comment;
